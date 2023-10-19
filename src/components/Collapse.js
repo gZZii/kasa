fc620cb8ed852f13ae978693e9ea9336dc9cb222
gZii <guillaume.adelet@gmail.com>
2023-10-19 16:15:33 +0200

    Ajout des animations aux extensions

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,31 +1,33 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-import vector from "../assets/img/vector.png"
+import { useState,useEffect } from 'react';
+import vector from "../assets/img/vector.png";
 
 const Collapse = (props) => {
-
     const [open, setOpen] = useState(false);
+    const [rotate, setRotate] = useState(false);
 
     const toggle = () => {
         setOpen(!open);
+        setRotate(!rotate);
     };
+
+    const vectorClass = rotate ? "vector rotate180" : "vector";
+    const contentClass = open ? "collapse-content open" : "collapse-content";
 
     return (
         <div className='collapse'>
             <div className='collapse-bandeau'>
                 <h3 className='collapse-title'>{props.label}</h3>
-                <img 
-                    src={vector} 
-                    alt="icone d'ouverture/fermeture du collapse" 
-                    onClick={toggle} 
-                    className='vector'
+                <img
+                    src={vector}
+                    alt="icone d'ouverture/fermeture du collapse"
+                    onClick={toggle}
+                    className={vectorClass}
                 />
             </div>
-                {open && (
-                    <div className='collapse-content'>
-                        <p>{props.text}</p>
-                    </div>
-                )}
+            <div className={contentClass}>
+                <p>{props.text}</p>
+            </div>
         </div>
     );
 };
