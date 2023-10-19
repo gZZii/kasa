00dbc5b67aa89faa04b79ae33c3ebce285cba149
gZii <guillaume.adelet@gmail.com>
2023-10-19 12:27:59 +0200

    création collapses et du style (sans animation)

--- src/components/Collapse.js
+++ src/components/Collapse.js
@@ -1,17 +1,31 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
+import vector from "../assets/img/vector.png"
 
 const Collapse = (props) => {
 
+    const [open, setOpen] = useState(false);
 
+    const toggle = () => {
+        setOpen(!open);
+    };
 
-
-
-
-    
     return (
         <div className='collapse'>
-            
+            <div className='collapse-bandeau'>
+                <h3 className='collapse-title'>{props.label}</h3>
+                <img 
+                    src={vector} 
+                    alt="icone d'ouverture/fermeture du collapse" 
+                    onClick={toggle} 
+                    className='vector'
+                />
+            </div>
+                {open && (
+                    <div className='collapse-content'>
+                        <p>{props.text}</p>
+                    </div>
+                )}
         </div>
     );
 };
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import Banner from '../components/Banner';
-import aboutBackgroundBanner from '../assets/img/img-banner-about.png'
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Banner from "../components/Banner";
+import aboutBackgroundBanner from "../assets/img/img-banner-about.png";
+import Collapse from "../components/Collapse";
 
 const About = () => {
-    return (
-        <div>
-            <Header />
-            <Banner img={aboutBackgroundBanner} />
-            <Footer />
-        </div>
-    );
+  let reliability = {
+    label: "Fiabilité",
+    text: "Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.",
+  };
+
+  let respect = {
+    label: "Respect",
+    text: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+  };
+
+  let service = {
+    label: "Service",
+    text: "La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.",
+  };
+
+  let security = {
+    label: "Sécurité",
+    text: "La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.",
+  };
+
+  return (
+    <div>
+      <Header />
+      <Banner img={aboutBackgroundBanner} />
+      <Collapse {...reliability} />
+      <Collapse {...respect} />
+      <Collapse {...service} />
+      <Collapse {...security} />
+      <Footer />
+    </div>
+  );
 };
 
 export default About;
